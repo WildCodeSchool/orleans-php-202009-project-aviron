3b30d3e1c4b6af5a3ca000dcf0ea5078c6d8e6c3
Barbara Gonthier <barbara.gonthier@gmail.com>
2020-12-08 16:54:28 +0100

    added subscribers fixtures

---
+++ src/DataFixtures/SubscriberFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Subscriber;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+class SubscriberFixtures extends Fixture
+{
+    private const GENDER = [
+        'F',
+        'H',
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+        for ($i = 0; $i < 100; $i++) {
+            $subscriber = new Subscriber();
+            $subscriber->setFirstname($faker->firstNameMale);
+            $subscriber->setLastname($faker->lastName);
+            $subscriber->setBirthdate(\DateTime::createFromFormat('Y-m-d', "1980-09-09"));
+            $subscriber->setLicenceNumber($faker->randomNumber(5, false));
+            $subscriber->setGender(self::GENDER[rand(0, 1)]);
+
+            $manager->persist($subscriber);
+            $this->addReference('subscriber_' . $i, $subscriber);
+        }
+        $manager->flush();
+    }
+}
