eae30fa935a86ad2cbfcb116ce89e8399cff33ba
Matthieu <bardetmatthieu@posteo.net>
2020-12-16 15:12:50 +0100

    display actual subscribers on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,49 @@
 
 namespace App\Controller;
 
+use App\Repository\LicenceRepository;
+use App\Repository\SeasonRepository;
+use App\Repository\SubscriptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
+    /**
+     * @var SubscriptionRepository
+     */
+    private SubscriptionRepository $repository;
+
+    public function __construct(SubscriptionRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     /**
      * @Route("/", name="home")
+     * @param SeasonRepository $season
+     * @param LicenceRepository $licence
+     * @return Response
      */
-    public function index(): Response
+    public function index(SeasonRepository $season, LicenceRepository $licence): Response
     {
-        return $this->render('home/index.html.twig');
+        $actualSeason = $this->getActualSeason($season);
+        $licenceAcronym = $this->getLicenceAcronym($licence);
+        $actualSubscribers = $this->repository->findAllSubscribersForActualSeason($licenceAcronym, $actualSeason);
+        return $this->render('home/index.html.twig', ['actualSubscribers' => $actualSubscribers]);
+    }
+
+    private function getActualSeason(SeasonRepository $season): ?string
+    {
+        $actualSeason = $season->findBy([], ['name' => 'DESC'], 1);
+        return $actualSeason[0]->getName();
+    }
+
+    private function getLicenceAcronym(LicenceRepository $licence): ?string
+    {
+        $competitionLicence = $licence->findBy(['acronym' => 'A']);
+        return $competitionLicence[0]->getAcronym();
     }
 }
--- src/Repository/SubscriptionRepository.php
+++ src/Repository/SubscriptionRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Subscription;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\AST\Join;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -17,6 +18,25 @@ class SubscriptionRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Subscription::class);
+    }
+
+    /**
+     * @param string|null $licenceAcronym
+     * @param string|null $seasonName
+     * @return Subscription
+     */
+    public function findAllSubscribersForActualSeason(?string $licenceAcronym, ?string $seasonName): Subscription
+    {
+        return $this->createQueryBuilder('sub')
+            ->select('COUNT(sub.subscriber)')
+            ->innerJoin('App\Entity\Licence', 'l', 'WITH', 'l.id = sub.licence')
+            ->innerJoin('App\Entity\Season', 's', 'WITH', 's.id = sub.season')
+            ->where('l.acronym = :licenceAcronym')
+            ->setParameter('licenceAcronym', $licenceAcronym)
+            ->andWhere('s.name = :seasonName')
+            ->setParameter('seasonName', $seasonName)
+            ->getQuery()
+            ->getResult();
     }
 
     // /**
