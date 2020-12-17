f18ded6929f7265031cf08c87e5a24960dfb8a50
Barbara Gonthier <barbara.gonthier@gmail.com>
2020-12-17 09:04:15 +0100

    added function in controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\SeasonRepository;
+use App\Repository\SubscriptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +13,17 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(SeasonRepository $seasonRepository, SubscriptionRepository $subscriptionRepo): Response
     {
-        return $this->render('home/index.html.twig');
+        $season = $this->getCurrentSeason($seasonRepository);
+        $countByLicences = $subscriptionRepo->subscribersByYearByLicences($season);
+        return $this->render('home/index.html.twig', [
+            'subscribersByLicences' => $countByLicences
+        ]);
+    }
+    private function getCurrentSeason(SeasonRepository $seasonRepository): ?string
+    {
+        $currentSeason = $seasonRepository->findBy([], ['name' => 'DESC']);
+        return $currentSeason[0]->getName();
     }
 }
