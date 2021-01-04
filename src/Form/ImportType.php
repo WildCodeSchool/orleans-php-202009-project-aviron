0fdfbed7811732b5d4811bfd7c33b7d57ba8b438
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2021-01-04 07:01:42 +0100

    add form

---
+++ src/Form/ImportType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Import;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImportType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('seasonName', TextType::class, [
+                'label' => 'Saison :'
+            ])
+            ->add('file', FileType::class, [
+                'label' => 'Fichier :'
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Import::class,
+        ]);
+    }
+}
