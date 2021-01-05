e402916484312da0dd4652edbf476483d41ca0fa
Barbara Gonthier <barbara.gonthier@gmail.com>
2021-01-05 11:19:37 +0100

    export with licences

--- src/Controller/SubscriberController.php
+++ src/Controller/SubscriberController.php
@@ -3,13 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\Filter;
+use App\Entity\Subscriber;
 use App\Form\FilterType;
 use App\Repository\SeasonRepository;
 use App\Repository\SubscriberRepository;
+use App\Service\Export;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @Route("/subscribers", name="subscribers_")
@@ -17,7 +23,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class SubscriberController extends AbstractController
 {
     /**
-     * @Route("/{display}/filter", name="filter")
+     * @Route("/{display}/filter/", name="filter")
      * @param string $display
      * @param Request $request
      * @param SubscriberRepository $subscriberRepository
@@ -42,10 +48,35 @@ class SubscriberController extends AbstractController
             return $this->render('subscriber/index.html.twig', [
                 'display' => $display,
                 'subscribers' => $subscribers,
-                'seasons' => $seasons
+                'seasons' => $seasons,
+                'filters' => $filters
             ]);
         }
 
         return $this->render('subscriber/filter.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/export/{display}", name="export")
+     * @param string $display
+     * @param SubscriberRepository $subscriberRepository
+     * @param SeasonRepository $seasonRepository
+     * @return Response
+     */
+    public function export(
+        string $display,
+        SubscriberRepository $subscriberRepository,
+        SeasonRepository $seasonRepository
+    ) {
+        $subscribers = $subscriberRepository->findAll();
+        $seasons = $seasonRepository->findAll();
+        $response = new Response($this->renderView('subscriber/export.csv.twig', [
+            'subscribers' => $subscribers,
+            'seasons' => $seasons,
+            'display' => $display,
+        ]));
+        $response->headers->set('Content-type', 'text/csv');
+        $response->headers->set('Content-Disposition', 'attachment; filename="export.csv"');
+        return $response;
     }
 }
