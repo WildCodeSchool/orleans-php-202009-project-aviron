fcb7f15e67da420c636a9cd25895b2f9e64b6f9b
Barbara Gonthier <barbara.gonthier@gmail.com>
2021-01-11 07:45:18 +0100

    added service colorCalculator and added property color to category

---
+++ src/Service/ColorCalculator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Service;
+
+class ColorCalculator
+{
+    public const COLOR = [
+        '#a65bd7aa' => ['SB', 'S-23', 'T','U'],
+        '#6688c3aa' => ['SA', 'S', 'N', 'A'],
+        '#37cf9baa' => ['B', 'J9', 'J10', 'J11', 'J12', 'R', 'D', 'D7', 'D30', 'D90'],
+        '#d56741aa' => ['J', 'J17', 'J18'],
+        '#f2e350aa' => ['M', 'J13', 'J14', 'R', 'I'],
+        '#e69138ff' => ['C', 'J15', 'J16'],
+        ];
+}
