9e8406bd3f280137368401857ca5ed507bfb262f
Barbara Gonthier <barbara.gonthier@gmail.com>
2021-01-11 09:46:45 +0100

    added colors to licenceFixtures

--- src/DataFixtures/LicenceFixtures.php
+++ src/DataFixtures/LicenceFixtures.php
@@ -9,20 +9,39 @@ use Doctrine\Persistence\ObjectManager;
 class LicenceFixtures extends Fixture
 {
     private const LICENCES = [
-        'A' => 'Compétition',
-        'U' => 'Universitaire',
-        'D7' => 'Découverte',
-        'D30' => 'Découverte',
-        'D90' => 'Découverte',
-        'I' => 'Indoor'
+        'A' => [
+            'name' => 'Compétition',
+            'color' => '#6688c3aa'
+            ],
+        'U' => [
+            'name' => 'Universitaire',
+            'color' => '#a65bd7aa'
+            ],
+        'D7' => [
+            'name' => 'Découverte',
+            'color' => '#37cf9baa'
+            ],
+        'D30' => [
+            'name' => 'Découverte',
+            'color' => '37cf9baa'
+            ],
+        'D90' => [
+            'name' => 'Découverte',
+            'color' => '#37cf9baa'
+            ],
+        'I' => [
+            'name' => 'Indoor',
+            'color' => '#f2e350aa'
+            ],
     ];
 
     public function load(ObjectManager $manager)
     {
         $index = 0;
-        foreach (self::LICENCES as $acronym => $name) {
+        foreach (self::LICENCES as $acronym => $data) {
             $licence = new Licence();
-            $licence->setName($name);
+            $licence->setName($data['name']);
+            $licence->setNColor($data['color']);
             $licence->setAcronym($acronym);
             $manager->persist($licence);
             $this->addReference('licence_' . $index, $licence);
