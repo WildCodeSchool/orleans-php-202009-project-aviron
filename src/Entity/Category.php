cc0954d3a7c18cd991dcd5af2d2ee5d512c65153
Barbara Gonthier <barbara.gonthier@gmail.com>
2021-01-12 09:32:02 +0100

    added regex

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -7,6 +7,7 @@ use App\Service\LabelInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CategoryRepository::class)
@@ -47,6 +48,7 @@ class Category implements LabelInterface
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\Regex("^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$")
      */
     private ?string $color;
 
