218b6500a431e468f63f7a6a1c62a88e9694b4cc
Barbara Gonthier <barbara.gonthier@gmail.com>
2021-01-18 11:35:39 +0100

    display user info

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,9 +4,17 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
+    public const ROLES = [
+        'ROLE_ADMIN' => "Administateur",
+        'ROLE_USER' => "Utilisateur",
+    ];
+        /**
+         * @Route("profile", name="profile")
+         */
     public function profile(): Response
     {
         return $this->render('home/profile.html.twig');
