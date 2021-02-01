3b78b4bcc1cc6316bcf4c08e079a2762332e903d
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2021-02-01 12:21:33 +0100

    PyramidFilterType extends FilterType

--- src/Form/PyramidFilterType.php
+++ src/Form/PyramidFilterType.php
@@ -14,78 +14,30 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class PyramidFilterType extends AbstractType
+class PyramidFilterType extends FilterType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        parent::buildForm($builder, $options);
         $builder
-            ->add('fromSeason', EntityType::class, [
-                'class' => Season::class,
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('s')
-                        ->orderBy('s.name', 'ASC');
-                },
-                'choice_label' => 'name',
-                'label' => 'De',
-                'error_bubbling' => true
-            ])
-            ->add('toSeason', EntityType::class, [
-                'class' => Season::class,
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('s')
-                        ->orderBy('s.name', 'DESC');
-                },
-                'choice_label' => 'name',
-                'label' => 'à',
-                'error_bubbling' => true
-            ])
-            ->add('gender', ChoiceType::class, [
-                'choices' => array_flip(Subscriber::GENDER),
-                'expanded' => true,
-                'multiple' => true,
-                'label' => false,
-                'required' => false,
-                'placeholder' => false,
-                'error_bubbling' => true,
-                'invalid_message' => "Le sexe choisi n'est pas une valeur valide"
-            ])
+            ->remove('fromAdherent')
+            ->remove('toAdherent')
+            ->remove('status')
+            ->remove('seasonStatus')
+            ->remove('licences')
+            ->remove('seasonLicence')
+            ->remove('seasonCategory')
+            ->remove('firstLicence')
+            ->remove('firstCategory')
+            ->remove('stillRegistered')
+            ->remove('duration')
+            ->remove('orMore')
+            ->remove('stillAdherent')
+
             ->add('newSubscriber', CheckboxType::class, [
                 'label' => 'Nouveaux uniquement',
                 'required' => false
-            ])
-            ->add('fromCategory', EntityType::class, [
-                'class' => Category::class,
-                'choice_label' => function ($category) {
-                    return $category->getLabel() . ' (' . $category->getOldGroup() . ')';
-                },
-                'choice_value' => function (?Category $entity) {
-                    return $entity ? $entity->getLabel() : '';
-                },
-                'group_by' => function ($choice, $key, $value) {
-                    return $choice->getNewGroup();
-                },
-                'placeholder' => '',
-                'label' => 'De',
-                'required' => false,
-                'error_bubbling' => true
-            ])
-            ->add('toCategory', EntityType::class, [
-                'class' => Category::class,
-                'choice_label' => function ($category) {
-                    return $category->getLabel() . ' (' . $category->getOldGroup() . ')';
-                },
-                'choice_value' => function (?Category $entity) {
-                    return $entity ? $entity->getLabel() : '';
-                },
-                'group_by' => function ($choice, $key, $value) {
-                    return $choice->getNewGroup();
-                },
-                'placeholder' => '',
-                'label' => 'à',
-                'required' => false,
-                'error_bubbling' => true
-            ])
-            ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
